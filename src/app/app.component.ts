d3000507ea7460fa80f038f75a7728111c06a817
jonathlan <38994980+jonathlan@users.noreply.github.com>
2022-10-29 22:38:38 +0200

    Add property biding example.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,11 @@ export class AppComponent {
   name = 'Diana San';
   age = 24;
   img = 'https://cdn.pixabay.com/photo/2015/12/23/06/36/vector-1105214_960_720.png';
+  btnDisabled = true;
+
+  person ={
+    name: 'Daniel',
+    age: 18,
+    avatar : 'https://cdn.pixabay.com/photo/2015/12/23/06/36/vector-1105214_960_720.png'
+  }
 }
