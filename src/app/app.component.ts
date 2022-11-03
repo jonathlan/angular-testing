c06388633dd4ff5c74b1cf5c8ca98843b5cb7627
jonathlan <38994980+jonathlan@users.noreply.github.com>
2022-11-03 20:34:00 +0100

    Add ngFor sample with arrays, using an interface product.model.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { ASTWithName } from '@angular/compiler';
 import { Component } from '@angular/core';
+import { Product } from './product.model';
 
 @Component({
   selector: 'app-root',
@@ -20,6 +21,40 @@ export class AppComponent {
 
   names: string[] = ['Angela', 'Valeria', 'Ricardo'];
   newName: string = '';
+
+  products: Product[] = [
+    {
+      name: 'Motorbike',
+      price: 3500,
+      image: 'https://cdn.pixabay.com/photo/2016/04/07/06/53/bmw-1313343_960_720.jpg',
+      category: 'Motorbikes'
+    },
+    {
+      name: 'Doraemon',
+      price: 500,
+      image: 'https://cdn.pixabay.com/photo/2018/12/23/09/45/toy-3890797_960_720.jpg'
+    },
+    {
+      name: 'Cuddly toy',
+      price: 250,
+      image: 'https://cdn.pixabay.com/photo/2016/03/26/22/22/happy-1281590_960_720.jpg'
+    },
+    {
+      name: 'Nutcracker',
+      price: 100,
+      image: 'https://cdn.pixabay.com/photo/2020/11/22/16/45/nutcracker-5767159_960_720.jpg'
+    },
+    {
+      name: 'Rubber duck',
+      price: 50,
+      image: 'https://cdn.pixabay.com/photo/2018/05/19/22/37/rubber-duck-3414512_960_720.jpg'
+    },
+    {
+      name: 'Lego Angel',
+      price: 25,
+      image: 'https://cdn.pixabay.com/photo/2013/06/01/18/01/angel-115544_960_720.jpg'
+    }
+  ]
 
   toggleButton(){
     this.btnDisabled = !this.btnDisabled;
